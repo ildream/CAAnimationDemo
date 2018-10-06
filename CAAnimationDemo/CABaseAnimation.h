345e7b4c1dde8a35f2f26202f01c4460f66eda8a
ildream <858642232@qq.com>
2018-10-06 23:12:33 +0800

    提交封装动画基础类

---
+++ CAAnimationDemo/CABaseAnimation.h
@@ -0,0 +1,13 @@
+//
+//  CABaseAnimation.h
+//  CAAnimationDemo
+//
+//  Created by 王安亚 on 2018/10/5.
+//  Copyright © 2018年 ay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CABaseAnimation : NSObject
+
+@end
