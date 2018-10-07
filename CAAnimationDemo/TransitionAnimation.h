364fe05fc55638c74499501e197018c5f2682885
ildream <858642232@qq.com>
2018-10-07 16:55:15 +0800

    提交TransitionAnimation

---
+++ CAAnimationDemo/TransitionAnimation.h
@@ -0,0 +1,17 @@
+//
+//  TransitionAnimation.h
+//  CAAnimationDemo
+//
+//  Created by ildream on 2018/10/7.
+//  Copyright © 2018年 ay. All rights reserved.
+//
+
+#import "CABaseAnimation.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TransitionAnimation : CABaseAnimation
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CAAnimationDemo/TransitionAnimation.m
@@ -0,0 +1,13 @@
+//
+//  TransitionAnimation.m
+//  CAAnimationDemo
+//
+//  Created by ildream on 2018/10/7.
+//  Copyright © 2018年 ay. All rights reserved.
+//
+
+#import "TransitionAnimation.h"
+
+@implementation TransitionAnimation
+
+@end
